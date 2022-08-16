7f3320a727c63bc07d34521d9807d24b315ca852
Tesfay-Teshome <tesfix2017@gmail.com>
2022-08-16 03:34:25 -0700

    Heap Sorting

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,51 +1,74 @@
 #include "sort.h"
 
 /**
-* stupify - recurrssive heapfiy function
-* @array: Array to sort
-* @heap: size of heap data
-* @i: index
-* @size: size of array
-*/
-
-void stupify(int *array, int heap, int i, int size)
+ * _swap - swaped 2 values.
+ * @array: the array for swap him values.
+ * @i: First index
+ * @j: Second index
+ * @r_size: The size constant for print
+ * Return: Nothing
+ */
+void _swap(int *array, int i, int j, const int r_size)
 {
-	int lar = i, left = 2 * i + 1, right = 2 * i + 2, t;
+	int tmp;
+	(void) r_size;
 
-	if (left < heap && array[left] > array[lar])
-		lar = left;
-	if (right < heap && array[right] > array[lar])
-		lar = right;
-	if (lar != i)
+	if (i != j)
 	{
-		t = array[i], array[i] = array[lar], array[lar] = t;
-		print_array(array, size);
-		stupify(array, heap, lar, size);
+		tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
+		print_array(array, (size_t)r_size);
 	}
 }
 
 /**
-* heap_sort - Sorts array with heap sort algo
-* @array: array to sort
-* @size: Size of array to sort
-*/
+ * _largest - Find the largest number btween the layers
+ * @array: The array for sort
+ * @size: The menor element
+ * @i: The largest.
+ * @r_size: The size for print in swap
+ * Return: Nothing.
+ */
+void _largest(int *array, size_t size, int i, const int r_size)
+{
+	int largest = i;
+	int lft = (2 * i) + 1;
+	int rgt = (2 * i) + 2;
 
+	if (lft < (int)size && array[lft] > array[largest])
+		largest = lft;
+
+	if (rgt < (int)size && array[rgt] > array[largest])
+		largest = rgt;
+
+	if (largest != i)
+	{
+		_swap(array, i, largest, r_size);
+		_largest(array, size, largest, r_size);
+	}
+}
+
+/**
+ * heap_sort - Call largest while exist layers
+ * @array: The array that generate the layers
+ * @size: Size of the array
+ * Return: Nothing
+ */
 void heap_sort(int *array, size_t size)
 {
-	int i = size / 2 - 1, temp;
+	const int r_size = (const int)size;
+	int i;
 
-	if (array == NULL || size < 2)
+	if (size < 2 || !array)
 		return;
-	for (; i >= 0; i--)
-		stupify(array, size, i, size);
+
+	for (i = size / 2 - 1; i >= 0; i--)
+		_largest(array, size, i, r_size);
+
 	for (i = size - 1; i >= 0; i--)
 	{
-		temp = array[0];
-		array[0] = array[i];
-		array[i] = temp;
-		if (i > 0)
-			print_array(array, size);
-		stupify(array, i, 0, size);
+		_swap(array, 0, i, r_size);
+		_largest(array, i, 0, r_size);
 	}
-
 }
